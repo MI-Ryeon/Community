f26e9237e15b2054031a346534820983bff8e40e
Ahn Jeongmin <symbol9550@gmail.com>
2023-07-19 17:23:36 +0900

    [관리자모드] 회원 개별 조회 기능 추가

--- src/main/java/com/sparta/community/controller/AdminController.java
+++ src/main/java/com/sparta/community/controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.sparta.community.controller;
 
 import com.sparta.community.dto.AdminUserListResponseDto;
+import com.sparta.community.dto.AdminUserResponseDto;
 import com.sparta.community.dto.ApiResponseDto;
 import com.sparta.community.entity.UserRoleEnum;
 import com.sparta.community.security.UserDetailsImpl;
 import com.sparta.community.service.AdminService;
+import com.sparta.community.service.CommentService;
 import com.sparta.community.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     private final AdminService adminService;
     private final PostService postService;
+    private final CommentService commentService;
 
     @GetMapping("/users")
     public ResponseEntity<AdminUserListResponseDto> getUsers() {
@@ -27,9 +30,22 @@ public class AdminController {
         return ResponseEntity.ok().body(result);
     }
 
+    @GetMapping("/users/{id}")
+    public ResponseEntity<AdminUserResponseDto> getUserById(@PathVariable Long id) {
+        AdminUserResponseDto result = adminService.getUserById(id);
+
+        return ResponseEntity.ok().body(result);
+    }
+
     @DeleteMapping("/posts/{id}")
     public ResponseEntity<ApiResponseDto> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
         postService.deletePost(id, userDetails.getUser());
         return ResponseEntity.ok().body(new ApiResponseDto("게시글 삭제 성공", HttpStatus.OK.value()));
+    }
+
+    @DeleteMapping("/comments/{id}")
+    public ResponseEntity<ApiResponseDto> deleteComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
+        commentService.deleteComment(id, userDetails.getUser());
+        return ResponseEntity.ok().body(new ApiResponseDto("댓글 삭제 성공", HttpStatus.OK.value()));
     }
 }
